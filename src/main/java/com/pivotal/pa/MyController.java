0ce44dfeb32ccefd3fb87007fa599d3cd4ed0655
Michael McNichol <mcnichol.m@gmail.com>
2018-09-08 00:20:19 -0500

    Expose JMX and Web endpoints, cleanup controller

--- src/main/java/com/pivotal/pa/MyController.java
+++ src/main/java/com/pivotal/pa/MyController.java
@@ -1,44 +1,32 @@
 package com.pivotal.pa;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
+
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.search.Search;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class MyController {
 
-    private MeterRegistry meterRegistry;
+    private Counter counter;
 
-    MyController(MeterRegistry meterRegistry){
-        this.meterRegistry = meterRegistry;
+    MyController(MeterRegistry meterRegistry) {
 
-        meterRegistry.getMeters();
-
-        ArrayList<Object> list = new ArrayList<>(4);
-
-        Counter
-                .builder("my-counter")
-                .description("indicates instance count of the object")
-                .tags("dev", "performance")
+        counter = Counter
+                .builder("my-custom-counter")
+                .description("instance count")
+                .tags("dev", "count")
                 .register(meterRegistry);
-
-        Gauge.builder("my-custom-cache", list, List::size).tag("dev","performance").register(meterRegistry);
     }
 
-    @Timed(value = "my-custom-timer", histogram = true, description = "hell-yes")
+    @Timed(value = "my-custom-timer", histogram = true)
     @RequestMapping("/")
     public ResponseEntity<String> myMetrics() {
-        Counter counter = meterRegistry.find("my-counter").counter();
         counter.increment();
 
-        return new ResponseEntity<>("all-is-good", HttpStatus.OK);
+        return new ResponseEntity<>("done", HttpStatus.OK);
     }
 }
